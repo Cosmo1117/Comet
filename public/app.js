d3cab681f471debeb589eb1afce4fa16a7a018c2
Bode Norman <normanbode224@gmail.com>
2026-03-20 22:27:08 -0400

    fix: fix scramjet loader

--- public/app.js
+++ public/app.js
@@ -147,10 +147,9 @@ console.log(Object.keys(window).filter(k => k.toLowerCase().includes('scramjet')
 console.log($scramjetLoadController);
 console.log(typeof $scramjetLoadController);
 
-const ScramjetController = $scramjetLoadController();
-console.log(ScramjetController);
-
 //sj
+
+const { ScramjetController } = $scramjetLoadController();
 
 const scramjet = new $scramjetLoadController({
     prefix: "/scramjet/",
